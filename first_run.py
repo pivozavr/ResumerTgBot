4404321abe76c89536752c860284039771738df1
Pivov <piv.artem@gmail.com>
2026-04-29 21:07:45 +0200

    commit

---
+++ first_run.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+connection = sqlite3.connect("messages.db")
+crsr = connection.cursor()
+sql_command = """CREATE TABLE messages (
+                chat_id INTEGER,
+                username VARCHAR(20),
+                text VARCHAR(65535),
+                date VARCHAR(65535));"""
+crsr.execute(sql_command)
+connection.commit()
+connection.close()
--- main.py
+++ main.py
@@ -12,16 +12,7 @@ import os
 from cryptography.fernet import Fernet
 
 
-# connection = sqlite3.connect("messages.db")
-# crsr = connection.cursor()
-# sql_command = """CREATE TABLE messages (
-#                 chat_id INTEGER,
-#                 username VARCHAR(20),
-#                 text VARCHAR(65535),
-#                 date VARCHAR(65535));"""
-# crsr.execute(sql_command)
-# connection.commit()
-# connection.close()
+
 
 
 load_dotenv()
